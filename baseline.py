f591edc87c25a985e9b85f02aed5a1020b9da79e
GGob2 <rkdaudwh13@naver.com>
2022-08-05 01:45:59 +0900

    result: using linear svr

--- baseline.py
+++ baseline.py
@@ -18,13 +18,14 @@ train_df = pd.read_csv('./dataset/train.csv')
 train_x = train_df.filter(regex='X') # Input : X Featrue
 train_y = train_df.filter(regex='Y') # Output : Y Feature
 
-SVR = MultiOutputRegressor(SVR(), n_jobs=-1).fit(train_x, train_y)# SVR을 이용한 Regression
+#SVR = MultiOutputRegressor(SVR(), n_jobs=-1).fit(train_x, train_y) SVR을 이용한 Regression
+LSVR = MultiOutputRegressor(LinearSVR(), n_jobs=-1).fit(train_x, train_y)
 #LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
 print('Done.')
 
 test_x = pd.read_csv('./dataset/test.csv').drop(columns=['ID'])
 
-preds = SVR.predict(test_x)
+preds = LSVR.predict(test_x)
 print('Done.')
 
 submit = pd.read_csv('./dataset/sample_submission.csv')
@@ -35,4 +36,4 @@ for idx, col in enumerate(submit.columns):
     submit[col] = preds[:,idx-1]
 print('Done.')
 
-submit.to_csv('./dataset/submit_svr.csv', index=False)
+submit.to_csv('./dataset/submit_lsvr.csv', index=False)
