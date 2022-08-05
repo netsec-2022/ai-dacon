68931cfbacbd7c2608ee44411f455ca0ebaf5d2d
GGob2 <rkdaudwh13@naver.com>
2022-08-06 00:30:40 +0900

    result: tuned xgb

--- baseline.py
+++ baseline.py
@@ -22,7 +22,8 @@ train_y = train_df.filter(regex='Y') # Output : Y Feature
 #SVR = MultiOutputRegressor(SVR(), n_jobs=-1).fit(train_x, train_y) SVR을 이용한 Regression
 #LSVR = MultiOutputRegressor(LinearSVR(), n_jobs=-1).fit(train_x, train_y)
 #LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
-XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma = 0, subsample=0.75, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y)
+#XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma = 0, subsample=0.75, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y)
+XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=1000, learning_rate=0.05, gamma = 0, subsample=0.75, colsample_bytree = 1, max_depth=8) ).fit(train_x, train_y)
 print('Done.')
 
 test_x = pd.read_csv('./dataset/test.csv').drop(columns=['ID'])
@@ -38,4 +39,4 @@ for idx, col in enumerate(submit.columns):
     submit[col] = preds[:,idx-1]
 print('Done.')
 
-submit.to_csv('./dataset/submit_xgb.csv', index=False)
+submit.to_csv('./dataset/submit_xgb_2.csv', index=False)
