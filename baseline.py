90100d501ffadc9365b487a536b0f4ccaf41d608
GGOB2 <rkdaudwh13@naver.com>
2022-08-07 02:43:30 +0900

    modify: learning_rate - 0.065

--- baseline.py
+++ baseline.py
@@ -23,7 +23,7 @@ train_y = train_df.filter(regex='Y') # Output : Y Feature
 #LSVR = MultiOutputRegressor(LinearSVR(), n_jobs=-1).fit(train_x, train_y)
 #LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
 #XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=150, learning_rate=0.07, gamma = 0, subsample=0.77, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
-XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=175, learning_rate=0.65, gamma = 0, subsample=0.79, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
+XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=175, learning_rate=0.065, gamma = 0, subsample=0.79, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
 
 print('Done.')
 
@@ -40,4 +40,4 @@ for idx, col in enumerate(submit.columns):
     submit[col] = preds[:,idx-1]
 print('Done.')
 
-submit.to_csv('./dataset/submit_xgb_4.csv', index=False)
+submit.to_csv('./dataset/submit_xgb.csv', index=False)
