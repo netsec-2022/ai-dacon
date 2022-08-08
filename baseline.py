e46c7cc0f396a041c60210f450498689e855cbe0
GGOB2 <rkdaudwh13@naver.com>
2022-08-09 02:17:54 +0900

    delete: rmse

--- baseline.py
+++ baseline.py
@@ -23,9 +23,8 @@ train_y = train_df.filter(regex='Y') # Output : Y Feature
 #SVR = MultiOutputRegressor(SVR(), n_jobs=-1).fit(train_x, train_y) SVR을 이용한 Regression
 #LSVR = MultiOutputRegressor(LinearSVR(), n_jobs=-1).fit(train_x, train_y)
 #LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
-#XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=150, learning_rate=0.07, gamma = 0, subsample=0.77, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
-XGB = MultiOutputRegressor(xgb.XGBRegressor(objective='reg:linear', n_estimators=50, learning_rate=0.1, alpha = 10, gamma = 0, subsample=0.77, colsample_bytree = 0.3, max_depth=5) ).fit(train_x, train_y) 
 
+XGB = MultiOutputRegressor(xgb.XGBRegressor(objective='reg:linear', n_estimators=150, learning_rate=0.07, gamma = 0, subsample=0.77, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
 
 print('Done.')
 
@@ -33,11 +32,6 @@ test_x = pd.read_csv('./dataset/test.csv').drop(columns=['ID'])
 
 preds = XGB.predict(test_x)
 print('Done.')
-
-print('calculate rmse...')
-
-rmse = np.sqrt(mean_squared_error(test_x, preds), squared=False)
-print("RMSE: %f" (rmse))
 
 submit = pd.read_csv('./dataset/sample_submission.csv')
 
