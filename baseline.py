26f5b3e5df700f895fd4fa9cc196cecf1df717d7
GGOB2 <rkdaudwh13@naver.com>
2022-08-09 02:14:55 +0900

    feat: new param

--- baseline.py
+++ baseline.py
@@ -7,6 +7,7 @@ import xgboost as xgb
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.svm import SVR, LinearSVR
+from sklearn.metrics import mean_squared_error
 
 def seed_everything(seed):
     random.seed(seed)
@@ -23,7 +24,8 @@ train_y = train_df.filter(regex='Y') # Output : Y Feature
 #LSVR = MultiOutputRegressor(LinearSVR(), n_jobs=-1).fit(train_x, train_y)
 #LR = MultiOutputRegressor(LinearRegression()).fit(train_x, train_y)
 #XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=150, learning_rate=0.07, gamma = 0, subsample=0.77, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
-XGB = MultiOutputRegressor(xgb.XGBRegressor(n_estimators=175, learning_rate=0.065, gamma = 0, subsample=0.79, colsample_bytree = 1, max_depth=7) ).fit(train_x, train_y) 
+XGB = MultiOutputRegressor(xgb.XGBRegressor(objective='reg:linear', n_estimators=50, learning_rate=0.1, alpha = 10, gamma = 0, subsample=0.77, colsample_bytree = 0.3, max_depth=5) ).fit(train_x, train_y) 
+
 
 print('Done.')
 
@@ -31,6 +33,11 @@ test_x = pd.read_csv('./dataset/test.csv').drop(columns=['ID'])
 
 preds = XGB.predict(test_x)
 print('Done.')
+
+print('calculate rmse...')
+
+rmse = np.sqrt(mean_squared_error(test_x, preds), squared=False)
+print("RMSE: %f" (rmse))
 
 submit = pd.read_csv('./dataset/sample_submission.csv')
 
